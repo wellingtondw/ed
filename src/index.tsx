b83c04a99ef8b91819fd65af7aed5e3c75933dda
Wellington <wellington.map@hotmail.com>
2022-06-23 19:27:43 -0300

    chore: add eslint and prettier to ensure code pattern

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,9 @@
-import React from "react";
-import ReactDOM from "react-dom";
+import React from 'react';
+import ReactDOM from 'react-dom';
 
 ReactDOM.render(
   <React.StrictMode>
     <h1>Hello World</h1>,
   </React.StrictMode>,
-  document.getElementById("root")
+  document.getElementById('root')
 );
